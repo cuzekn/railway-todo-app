0e9c891470a650064df9c4f9f30dab10d5959dc0
cuzekn <cuzekn@gmail.com>
2025-10-06 01:20:26 +0900

    chore: eslint, prettierを設定

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,12 +7,12 @@ export default defineConfig({
   server: {
     port: 3000,
     hmr: {
-      overlay: false
+      overlay: false,
     },
   },
   resolve: {
     alias: {
-      '~': '/src',
+      '@': '/src',
     },
   },
-})
+});
